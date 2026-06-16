fb028b81aa3fa8dc194a2811178f8d199da8009f
ruzicka <ruzicka365@gmail.com>
2026-06-16 16:31:21 +0200

    pmd fix

--- src/main/java/com/doomhamsters/gamesession/cardcommands/CardRegistry.java
+++ src/main/java/com/doomhamsters/gamesession/cardcommands/CardRegistry.java
@@ -4,7 +4,6 @@ import com.doomhamsters.Card;
 import com.doomhamsters.gamesession.cardcommands.cards.BegForSnacksCardDefinition;
 import com.doomhamsters.gamesession.cardcommands.cards.PowerNapCardDefinition;
 import com.doomhamsters.gamesession.cardcommands.cards.QuickPeekCardDefinition;
-import com.doomhamsters.gamesession.cardcommands.cards.StealCardDefinition;
 import com.doomhamsters.gamesession.dto.CardDto;
 import java.util.Collection;
 import java.util.Comparator;

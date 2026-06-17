b761bd62bcc69865b89af261e83341cff3f0e7b0
michelle-fe <michellefe@edu.aau.at>
2026-06-17 16:17:22 +0200

    test: correct starting-hand size to 17

--- src/test/java/com/doomhamsters/gamesession/GameControllerTest.java
+++ src/test/java/com/doomhamsters/gamesession/GameControllerTest.java
@@ -177,7 +177,7 @@ class GameControllerTest {
     assertEquals(31, testSession.getGame().getDeck().size());
     assertEquals(7, snackStashCardsInGame);
     testSession.getGame().getPlayers().forEach(player ->
-        assertEquals(16, player.getHand().size()));
+        assertEquals(17, player.getHand().size()));
   }
 
   @Test

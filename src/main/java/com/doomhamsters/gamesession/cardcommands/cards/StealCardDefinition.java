2238f59a2108e7f764a52f00bb8ad036eec75024
Olha Kupar <olhaku@edu.aau.at>
2026-06-24 17:06:24 +0200

    fix: Add a nested comment explaining why this method is empty, throw an UnsupportedOperationException or complete the implementation.

--- src/main/java/com/doomhamsters/gamesession/cardcommands/cards/StealCardDefinition.java
+++ src/main/java/com/doomhamsters/gamesession/cardcommands/cards/StealCardDefinition.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class StealCardDefinition implements CardDefinition {
 
-  public StealCardDefinition() {}
 
   @Override
   public String cardType() {

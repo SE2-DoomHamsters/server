c319545bec9fe070fb47538a342ed195d42762ea
Olha Kupar <olhaku@edu.aau.at>
2026-06-24 17:08:36 +0200

    fix: Fix the incompatibility of the annotation @Nullable to honor @NullMarked at package level of the overridden method.

--- src/main/java/com/doomhamsters/web/RestExceptionHandler.java
+++ src/main/java/com/doomhamsters/web/RestExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -74,7 +75,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
   @Override
   protected ResponseEntity<Object> handleExceptionInternal(
       Exception ex,
-      Object body,
+      @Nullable Object body,
       HttpHeaders headers,
       HttpStatusCode statusCode,
       WebRequest request) {
